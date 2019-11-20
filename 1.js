97c27ba6eef33c7292005e2bf7f16f11397351bc
DynamoVue <truongvuetify@gmail.com>
2019-11-20 13:10:25 +0700

    add arrow image

--- 1.js
+++ 1.js
@@ -4,6 +4,9 @@ var burgerLine = document.querySelectorAll('.burger > div');
 var navMenuItems = document.querySelectorAll('.nav-links > .nav-link');
 var leftArrow = document.querySelector('.laptop-select > .left');
 var rightArrow = document.querySelector('.laptop-select > .right');
+var backToTop = document.getElementsByClassName('page-backToTop');
+var header = document.getElementsByTagName('header');
+var pageContent = document.getElementsByClassName('page-content');
 burger.addEventListener('click',function(){
     navMenu.classList.toggle('nav-active');
     burger.classList.toggle('burger-break');
@@ -13,8 +16,19 @@ burger.addEventListener('click',function(){
         }else navMenuItem.style.animation = '';
     });
 })
+window.addEventListener('scroll',function(){
+    let positionScrolled = this.pageYOffset;
+    let AboutUs = document.getElementsByClassName('page-aboutUs');
+    let AboutUsPosition = AboutUs[0].offsetTop - 300;
+    if(positionScrolled > 150){
+        header[0].classList.add('fixed-menu');
+    } else header[0].classList.remove('fixed-menu');
+    this.console.log(AboutUsPosition);
+    if(positionScrolled > AboutUsPosition){
+        backToTop[0].classList.add('active');
+    } else backToTop[0].classList.remove('active');
+})
 
-var img = document.querySelectorAll('.item__img > img');
-img[0].onclick = function(){
-    location.reload();
-}
+backToTop[0].addEventListener('click',function(){
+    pageContent[0].scrollIntoView({behavior : "smooth", block: "start"});
+})
